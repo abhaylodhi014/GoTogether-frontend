56b11489421a20eccd6feb313bab47a4397aff77
abhaylodhi014 <abhaylodhi128135@gmail.com>
2025-02-18 16:27:00 +0530

    handle main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from './utils/ErrorBoundary';
 
 // Use createRoot for React 18+ rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
